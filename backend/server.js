8556f8473b4fc22cb4be071d438792dbb1760698
Amiin <amiinali29@gmial.com>
2026-06-07 10:05:42 +0300

    fixi

--- backend/server.js
+++ backend/server.js
@@ -35,8 +35,4 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
-});
+export default app;
